9f975718e3a5d9b4b493dcea22dcc928160462b4
DK\IRO <iro@sitecore.net>
2018-09-07 14:04:41 +0300

    Initial

---
+++ src/Sitecore.Support.253359/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.253359")]
+[assembly: AssemblyProduct("Sitecore.Support.253359")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.253359/XA/Foundation/Grid/Pipelines/DecorateRendering/GetGridCssClasses.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.Support.XA.Foundation.Grid.Pipelines.DecorateRendering
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Data.Items;
+  using Sitecore.DependencyInjection;
+  using Sitecore.XA.Foundation.Grid;
+  using Sitecore.XA.Foundation.Grid.Model;
+  using Sitecore.XA.Foundation.Grid.Parser;
+  using Sitecore.XA.Foundation.MarkupDecorator.Pipelines.DecorateRendering;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class GetGridCssClasses : RenderingDecorator
+  {
+    public override void Process(RenderingDecoratorArgs args)
+    {
+      Item gridDefinitionItem = ServiceLocator.ServiceProvider.GetService<IGridContext>().GetGridDefinitionItem(args.Rendering.Item, base.Context.Device);
+      if (gridDefinitionItem != null)
+      {
+        IGridFieldParser gridFieldParser = new GridDefinition(gridDefinitionItem).InstantiateGridFieldParser();
+        string value = args.Rendering.Parameters["GridParameters"];
+        List<string> list = gridFieldParser.ParseFromFieldValue(value).ToList();
+        if (list.Any())
+        {
+          args.AddAttribute("class", list);
+        }
+      }
+    }
+  }
+}
